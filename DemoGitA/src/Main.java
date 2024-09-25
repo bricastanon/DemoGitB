fe2675e4ad39e61aab9739437ac2a96bb0ce6aa4
Brianna Castanon <bricastanon1@gmail.com>
2024-09-25 14:13:44 -0700

    Commiting to stage

--- DemoGitA/src/Main.java
+++ DemoGitA/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // Create a variable that describes what you think of coding so far
         String iThinkCodingIs = "AWESOME!";
         boolean doYouGuysLikeItSOFar = true;
+        int secondAttempt = 2;
 
 
     }
