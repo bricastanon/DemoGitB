ef3f47279ab83e73e537922e2bc444aa4d7656d5
Brianna Castanon <bricastanon1@gmail.com>
2024-09-25 13:58:27 -0700

    Created a Variable

---
+++ DemoGitA/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Create a variable that describes what
+        String iThinkCodingIs = "AWESOME!";
+}
