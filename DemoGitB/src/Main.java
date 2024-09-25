fac86eaabdabd8bc5a88e76a2d141faae32e69e3
Brianna Castanon <bricastanon1@gmail.com>
2024-09-25 14:43:55 -0700

    String

--- DemoGitB/src/Main.java
+++ DemoGitB/src/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
         // Create a variable that describes what you think of coding so far
         String iThinkCodingIs = "AWESOME!";
-        boolean doYouGuysLikeItSOFar = true;
-        int secondAttempt = 2;
-        // this is a comment
-        // now staging and ready to put GITHUB online
-        // Update!
-
 
     }
 }
